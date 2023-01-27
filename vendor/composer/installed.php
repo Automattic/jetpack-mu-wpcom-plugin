d134316bb5ee1613b04f6a3eb9cc5390b3cd1b89
samiff <samiff@users.noreply.github.com>
2023-01-27 21:44:27 +0000

    Add plugin jetpack-mu-wpcom-plugin (#28479)
    * Add jetpack-mu-wpcom-plugin skeleton
    
    * Require the jetpack-mu-wpcom package
    
    * Add test bootstrap
    
    * Add mirror repo and other `extra` config
    
    * Refresh lock file
    
    * Add files to production-include
    
    * Add autoloader-suffix to composer.json
    
    * Fix autoloader-suffix version
    
    * Check out commit as well as trunk in post-build
    
    * Rename plugin directory to mu-wpcom-plugin
    
    * Bump node version
    
    * Update bug label name
    
    ---------
    
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4028299671

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automattic/jetpack-mu-wpcom-plugin',
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'reference' => NULL,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/jetpack-mu-wpcom' => array(
+            'pretty_version' => '0.1.1-alpha',
+            'version' => '0.1.1.0-alpha',
+            'reference' => 'de3d521f17e0feea9f1453add4923d61ba816863',
+            'type' => 'jetpack-library',
+            'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'automattic/jetpack-mu-wpcom-plugin' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'reference' => NULL,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
