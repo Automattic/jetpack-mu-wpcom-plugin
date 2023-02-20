6dfc6834ce55d932f8f7e352d2300bd0686c3dc0
samiff <samiff@users.noreply.github.com>
2023-02-20 20:33:03 +0000

    Updated package versions for automattic/jetpack
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4226665853

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '0.2.1',
-            'version' => '0.2.1.0',
-            'reference' => '721b9c03be22f7b69dd071ba9b6652654c9d3e93',
+            'pretty_version' => '0.2.2',
+            'version' => '0.2.2.0',
+            'reference' => 'c869c6aae10073d5446d8010d9feb7243ec12b48',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
