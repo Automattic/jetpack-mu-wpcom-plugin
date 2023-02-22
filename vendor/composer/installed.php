8f141af02604cd22c79ab43e5583dd61680a1be6
samiff <samiff@users.noreply.github.com>
2023-02-22 16:23:25 +0000

    jetpack-mu-wpcom: updated checks for coming-soon (#28932)
    * jetpack-mu-wpcom: updated checks for coming-soon
    
    * Fixup project versions
    
    * Try major version bump
    
    * Fixup project versions
    
    * More fixup project versions
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4244572778

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '0.2.2',
-            'version' => '0.2.2.0',
-            'reference' => 'c869c6aae10073d5446d8010d9feb7243ec12b48',
+            'pretty_version' => '0.2.3-alpha',
+            'version' => '0.2.3.0-alpha',
+            'reference' => '43700b1a981502d72ccfa689df6e0c36d1bb95bd',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
