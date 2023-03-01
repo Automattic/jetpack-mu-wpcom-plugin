8429370ab104116cce7aabb830e8909f6c7fe454
mattwiebe <mattwiebe@users.noreply.github.com>
2023-03-01 17:15:08 +0000

    Launchpad: add REST API endpoint (#29082)
    This contains:
    
    1. Infra for loading the same endpoint on both Simple and Atomic.
    2. A Launchpad endpoint - see #73830
    
    For 1), we still need to map the passthrough in `jetpack-bridge.php` but perhaps that could be done automatically, for endpoints here, in the future.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4305972455

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -8,4 +8,5 @@ $baseDir = dirname($vendorDir);
 return array(
     'Automattic\\Jetpack\\Jetpack_Mu_Wpcom' => $vendorDir . '/automattic/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php',
     'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
+    'WPCOM_REST_API_V2_Endpoint_Launchpad' => $vendorDir . '/automattic/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad.php',
 );
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '1.0.1',
-            'version' => '1.0.1.0',
-            'reference' => '253bf592330807fddb7fd6360968c38a047159ea',
+            'pretty_version' => '1.1.0-alpha',
+            'version' => '1.1.0.0-alpha',
+            'reference' => 'c63914781f763f4263f4a4ef621c9a2bdff0c159',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
