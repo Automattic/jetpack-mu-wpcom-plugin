caff8a02dd59c77943f393dae7c010fa860df389
sdixon194 <sdixon194@users.noreply.github.com>
2023-03-08 17:54:13 +0000

    Backport: Jetpack 12.0-a.1 changes (#29363)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4366917568

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,9 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## 1.0.7-alpha - unreleased
+## 1.0.8-alpha - unreleased
 
 This is an alpha version! The changes listed here are not final.
+
+## 1.0.7 - 2023-03-08
+### Changed
+- Minor internal updates.
 
 ## 1.0.6 - 2023-02-28
 ### Changed
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '1.1.0-alpha',
-            'version' => '1.1.0.0-alpha',
-            'reference' => 'c63914781f763f4263f4a4ef621c9a2bdff0c159',
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '35c65f59f54b2c0612f2e307ebffa0fb0ab4731a',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
