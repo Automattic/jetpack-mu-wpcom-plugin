1473491803988d5be1c02d74868d843abb40908c
sdixon194 <sdixon194@users.noreply.github.com>
2023-03-13 18:04:08 +0000

    Updated package versions for automattic/jetpack-mu-wpcom-plugin
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4407951300

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => 'v1.1.0',
-            'version' => '1.1.0.0',
-            'reference' => 'e64998c777c6b5cbf3367cd3404647f5d8525eff',
+            'pretty_version' => 'v1.1.2',
+            'version' => '1.1.2.0',
+            'reference' => 'c85f93e9b1dee015a33515bdcc1a8ea51cf985f7',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
