40f1faf5bbab42a1ca574aa52314d0e778d7c0ef
agrullon95 <agrullon95@users.noreply.github.com>
2023-04-03 14:29:17 +0000

    Update/launchpad checklist tasks statuses site settings (#29841)
    * Add update launchpad site settings endpoint
    
    * changelog
    
    * Updated update site options function name and comment
    
    * Update launchpad update_site_options logic
    
    * Update version
    
    * Update composer lock
    
    * changelog
    
    * Update composer lock
    
    * Update project versions
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4597674206

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,9 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## 1.0.13-alpha - unreleased
+## 1.1.0-alpha - unreleased
 
 This is an alpha version! The changes listed here are not final.
+
+### Added
+- Launchpad: Update composer.lock
 
 ## 1.0.12 - 2023-03-28
 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '1.2.2-alpha',
-            'version' => '1.2.2.0-alpha',
-            'reference' => '2512e9328eb29dbcb38619775ebea95ce419afe5',
+            'pretty_version' => '1.3.0-alpha',
+            'version' => '1.3.0.0-alpha',
+            'reference' => '1a65cfd42adf33bdea1d70552bb98aabc4402e6d',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
