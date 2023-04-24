3f4c670bccd07bb88301d4254070630be421fa93
jeyip <jeyip@users.noreply.github.com>
2023-04-24 18:42:00 +0000

    Launchpad Checklist API: Add checklist and task registry (#30211)
    * Add initial draft of checklist registry
    
    * changelog
    
    * Add task registry
    
    * Add build method
    
    * Trigger validation errors
    
    * Show errors with _doing_it_wrong method
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4789725818

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'Automattic\\Jetpack\\Jetpack_Mu_Wpcom' => $vendorDir . '/automattic/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php',
     'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
+    'Launchpad_Task_Lists' => $vendorDir . '/automattic/jetpack-mu-wpcom/src/features/launchpad/class-launchpad-task-lists.php',
     'WPCOM_REST_API_V2_Endpoint_Launchpad' => $vendorDir . '/automattic/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad.php',
     'WPCOM_REST_API_V2_Endpoint_Launchpad_Checklist' => $vendorDir . '/automattic/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad-checklist.php',
 );
