47fd609c9af33834edf67185bea065f9c801dfd2
zinigor <zinigor@users.noreply.github.com>
2023-04-25 17:06:43 +0000

    Starting new alpha cycles for Jetpack, Social, and Backup. (#30289)
    * Changelog edits.
    
    * Amend readme.txt
    
    * Starting Jetpack 12.2-a.0.
    
    * Starting Jetpack Social 1.10.1-alpha.
    
    * Starting Jetpack Backup 1.8-alpha.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4800063777

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '1.5.0-alpha',
-            'version' => '1.5.0.0-alpha',
-            'reference' => 'f9e9e9c602c27742f831be358bd9ba62e5444b5e',
+            'pretty_version' => '1.5.0',
+            'version' => '1.5.0.0',
+            'reference' => 'd2a822bc743969112f2111540a7b2123eb0f9c1c',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
