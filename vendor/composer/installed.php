7bfeceffb5e96ae6429ee647d53423bfcf7fcfbd
anomiex <anomiex@users.noreply.github.com>
2023-05-08 16:54:49 +0000

    Backport changelogs from Jetpack 12.2-a.3 release (#30511)
    * Changelog edits.
    * Init Jetpack 12.2-a.4
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4917437603

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '1.8.0-alpha',
-            'version' => '1.8.0.0-alpha',
-            'reference' => 'a88592b02525dc35c50dc8ccceefb0c140ab6a72',
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => '253912e432e5c617ead29c89e923646d5b1917d1',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
