99c9f382ac766fa980ddb05431d2d2b5579459bd
zinigor <zinigor@users.noreply.github.com>
2023-06-14 17:18:37 +0000

    Added project versioning suffixes. (#31220)
    * Added project versioning suffixes.
    
    * Update tools/cli/commands/build.js
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    * Update tools/cli/commands/build.js
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    * Added a separate property for the run version.
    
    * Implemented Brad's solution that would eliminate differing run version numbers.
    
    * Update tools/cli/commands/build.js
    
    * Update tools/cli/commands/build.js
    
    * Moved the run version block away from the line handler function.
    
    ---------
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5270004568

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '3.1.0-alpha',
-            'version' => '3.1.0.0-alpha',
-            'reference' => 'e877be9874213e4ead79cfe6e881b6ccac6217c1',
+            'pretty_version' => '3.1.0-alpha.1686762034',
+            'version' => '3.1.0.0-alpha1686762034',
+            'reference' => 'bd91be3402e8c7c573c9b9a65fc52b723c0f3fcb',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
