41e38be1364596947816aa49441a3cd911733b3c
pablinos <pablinos@users.noreply.github.com>
2023-06-14 19:41:53 +0000

    Tock Block: Hardcode the HTML code for PHP rendering (#31385)
    On WPCOM we strip data attributes to prevent XSS vulnerabilities from
    unsanitized user data being injected in to the HTML.
    
    For the Tock block to function properly it needs these attrbutes, so we
    can hardcode the HTML for the embed in the PHP render_block function.
    
    Co-authored-by: Samiff <samiff@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5271297002

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '3.1.0-alpha.1686767698',
-            'version' => '3.1.0.0-alpha1686767698',
-            'reference' => 'ed3af5d3e2721087483f09f1ea5f54a636de98d5',
+            'pretty_version' => '3.1.0-alpha.1686770710',
+            'version' => '3.1.0.0-alpha1686770710',
+            'reference' => '7ebcbc134863cedf17e6e9f6223a37a38d1b41f8',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
