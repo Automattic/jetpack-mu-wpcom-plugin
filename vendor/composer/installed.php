69ca4e156dd3a0194d52a02f649c9c6e9c08af6a
samiff <samiff@users.noreply.github.com>
2023-06-14 18:54:56 +0000

    Jetpack: backport 12.2.1 release (#31388)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5270877635

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '3.1.0-alpha.1686764836',
-            'version' => '3.1.0.0-alpha1686764836',
-            'reference' => 'b4d39b7bb669e779e6b823130f7dd5c80f9f07f8',
+            'pretty_version' => '3.1.0-alpha.1686767698',
+            'version' => '3.1.0.0-alpha1686767698',
+            'reference' => 'ed3af5d3e2721087483f09f1ea5f54a636de98d5',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
