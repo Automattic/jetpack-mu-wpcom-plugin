8ab933f625780f543daa96236f4b7d5cc33a010d
retrofox <retrofox@users.noreply.github.com>
2023-06-15 23:04:25 +0000

    AI Assistant: extract and use block content utils (#31409)
    * [not verified] extract block content util functions
    
    * use block content utils
    
    * rename debug id
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5284313265

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '3.1.0-alpha.1686868081',
-            'version' => '3.1.0.0-alpha1686868081',
-            'reference' => '4e35c11952673b5ca3b261d727896c8ed474b274',
+            'pretty_version' => '3.1.0-alpha.1686869322',
+            'version' => '3.1.0.0-alpha1686869322',
+            'reference' => 'f7c4da0685754911464a7e582739cd78c7bc5702',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
