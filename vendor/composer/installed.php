85f27d8d4f8bb81442bae42b0c36a332e56c14ca
retrofox <retrofox@users.noreply.github.com>
2023-06-15 11:39:34 +0000

    AI Assistant: Add translate action in extension (#31384)
    * AI Assistant: Add translate into extensions
    
    * changelog
    
    * AI Assistant: Export new I18n menu dropdown
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5278190096

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '3.1.0-alpha.1686823800',
-            'version' => '3.1.0.0-alpha1686823800',
-            'reference' => 'bca98f2eccfba305c8c3ca59ff899a9b6e3896e8',
+            'pretty_version' => '3.1.0-alpha.1686828205',
+            'version' => '3.1.0.0-alpha1686828205',
+            'reference' => 'fd6cb5ab0a3d4c0238ada742604d92123bda012e',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
