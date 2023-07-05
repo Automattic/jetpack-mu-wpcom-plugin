6f2a030b2874d874ef9b702a087f776c99293844
paulopmt1 <paulopmt1@users.noreply.github.com>
2023-07-05 20:35:46 +0000

    Deletes hello-world default post for users at design-first flow (#31702)
    * [not verified] While in the design_first flow, if the user creates a post, deletes the default hello-world.
    
    * [not verified] Strict in_array validation added
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5468535647

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '3.5.0',
-            'version' => '3.5.0.0',
-            'reference' => 'a0edc3f1f26425f97002590e6172fd6095c0cd7b',
+            'pretty_version' => '3.6.0-alpha.1688588105',
+            'version' => '3.6.0.0-alpha1688588105',
+            'reference' => '68bc731094d31b25ee0b5443d5ed187fb3dcbf52',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
