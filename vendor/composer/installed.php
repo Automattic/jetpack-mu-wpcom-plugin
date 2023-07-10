5722c39310b25f6eb5bc7813528fce05c5ab825d
nelsonec87 <nelsonec87@users.noreply.github.com>
2023-07-10 15:10:25 +0000

    Launchpad: removed edit site task from keepbuilding (#31759)
    * Removed edit site task from keep-building checklist
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5509565297

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '3.6.0-alpha.1688648931',
-            'version' => '3.6.0.0-alpha1688648931',
-            'reference' => '2843923b0626b834be8c6488139a49cad880f949',
+            'pretty_version' => '3.6.0-alpha.1689000589',
+            'version' => '3.6.0.0-alpha1689000589',
+            'reference' => 'a34b9439b492274aebc759549ed5189cee107e12',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
