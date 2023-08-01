498f03c4f28d7e0deb8ea1de6844e1349c3ef857
zinigor <zinigor@users.noreply.github.com>
2023-08-01 13:22:05 +0000

    [not verified] General: update readme for 12.4 release (#32172)
    See p1690826907411799-slack-C01U2KGS2PQ
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5727010285

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automattic/jetpack-mu-wpcom-plugin',
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'reference' => NULL,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/jetpack-mu-wpcom' => array(
+            'pretty_version' => 'v4.0.0',
+            'version' => '4.0.0.0',
+            'reference' => '3a9c5afcd995edb2447435dd3339ebd1d7964bd0',
+            'type' => 'jetpack-library',
+            'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'automattic/jetpack-mu-wpcom-plugin' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'reference' => NULL,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
