5771e87b047e4b9488feccf7424f09918515f7f3
danielbachhuber <danielbachhuber@users.noreply.github.com>
2023-09-07 12:33:59 +0000

    WPCOM Media: Add HEIC/HEIF image upload support (#32900)
    * WPCOM Media: Add HEIC/HEIF image upload support
    
    * changelog
    
    ---------
    
    Co-authored-by: Wojtek Naruniec <wojtek@naruniec.me>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6109502860

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '4.8.0-alpha.1694077071',
-            'version' => '4.8.0.0-alpha1694077071',
-            'reference' => '016e16c97a3d77224474d71fbd508ce51380de8c',
+            'pretty_version' => '4.8.0-alpha.1694088618',
+            'version' => '4.8.0.0-alpha1694088618',
+            'reference' => '082f832e3b61d7a879234ab50ebc60ea1367ca24',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
