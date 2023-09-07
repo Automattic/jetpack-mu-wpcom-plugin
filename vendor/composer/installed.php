e5b6c80fa9a8b3f8b314d2fbc90790a5a4895bd4
danielbachhuber <danielbachhuber@users.noreply.github.com>
2023-09-07 13:13:28 +0000

    Release mu-wpcom-plugin v1.6.18
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6109947173

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '4.8.0-alpha.1694088618',
-            'version' => '4.8.0.0-alpha1694088618',
-            'reference' => '082f832e3b61d7a879234ab50ebc60ea1367ca24',
+            'pretty_version' => '4.8.0',
+            'version' => '4.8.0.0',
+            'reference' => '29fd32eee87ea69a7a51eeecb5c15cf245c59f01',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
