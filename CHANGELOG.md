e2246ea4abae2847bc58f3e609ff25f540e171f2
zinigor <zinigor@users.noreply.github.com>
2023-09-11 15:02:27 +0000

    Updated package versions for automattic/jetpack-social
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6148082396

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,12 +5,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## 1.6.19-alpha - unreleased
-
-This is an alpha version! The changes listed here are not final.
-
+## 1.6.19 - 2023-09-11
 ### Changed
-- Updated package dependencies.
+- Updated package dependencies. [#32966]
 
 ## 1.6.18 - 2023-09-07
 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '4.9.0-alpha.1694387327',
-            'version' => '4.9.0.0-alpha1694387327',
-            'reference' => '75d722764e68beb54018264dd2a42f9bd7f6000e',
+            'pretty_version' => 'v4.9.0',
+            'version' => '4.9.0.0',
+            'reference' => 'd3c91caa854173df52f3d9b6613db6473e0dcfc8',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
