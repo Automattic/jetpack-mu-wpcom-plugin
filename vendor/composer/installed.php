1bd074646ab83eb12670eab56d2e74390b5f5f7f
zinigor <zinigor@users.noreply.github.com>
2023-09-19 20:54:43 +0000

    Fix trunk versions. (#33192)
    * Fixes trunk versions.
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6240641799

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '4.11.0-alpha.1695148990',
-            'version' => '4.11.0.0-alpha1695148990',
-            'reference' => 'a3fe00acee502f53cd02a6b3b0278ec44618f7f4',
+            'pretty_version' => '4.11.0-alpha.1695155576',
+            'version' => '4.11.0.0-alpha1695155576',
+            'reference' => '2182a1d4b44c2327cb20a3cb4ef5e80281fc494f',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
