c1f2835aac8c1e41a591796f340ceb8c66a832ba
mpkelly <mpkelly@users.noreply.github.com>
2023-10-02 06:17:40 +0000

    Frontend email nag for custom domain  (#33390)
    * add plugin to show email nag on the frontend
    
    * show frontend domain email nag
    
    * add changelog
    
    * fix doc
    
    * Update domain-email-nag.php
    
    Add slash to do class look-up in global namespace instead of relative to package (it was causing an error on the frontend for me)
    
    * Rework logic to use Domain_Management class; change backround to red
    
    * Use the correct URL
    
    ---------
    
    Co-authored-by: Michael Kelly <mike.kelly@a8c.com>
    Co-authored-by: mpkelly <kikemelly@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6376528204

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '4.13.0-alpha.1696224944',
-            'version' => '4.13.0.0-alpha1696224944',
-            'reference' => 'dcea782b53e7f3c95a2566ea37fa5a82b5272f38',
+            'pretty_version' => '4.13.0-alpha.1696226389',
+            'version' => '4.13.0.0-alpha1696226389',
+            'reference' => '88468d6961398295e57e3e4126c395e478d17c65',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
