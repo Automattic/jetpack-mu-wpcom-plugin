07c94af61fb4a204416d4ae0a9978f76e7c89bf2
edanzer <edanzer@users.noreply.github.com>
2023-10-12 04:13:42 +0000

    Launchpad: Add earn checklist (#33200)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6491005619

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '4.15.0-alpha.1697016002',
-            'version' => '4.15.0.0-alpha1697016002',
-            'reference' => 'c7511e524a0320008efdf8ed77e99666008e282b',
+            'pretty_version' => '4.15.0-alpha.1697082496',
+            'version' => '4.15.0.0-alpha1697082496',
+            'reference' => '8afc08eb49e3f5167f3a039f3d198d9c3bf57221',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
