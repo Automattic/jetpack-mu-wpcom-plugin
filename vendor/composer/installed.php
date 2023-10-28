b14ff5985cfc31a58df34bbb3f26ddb49ec2fe8c
vykes-mac <vykes-mac@users.noreply.github.com>
2023-10-28 01:46:12 +0000

    Add launchpad checklist for sites created in the hosting flow. (#33698)
    * add hosting flow home checklist tasks
    
    * add function to mark plugin installed
    
    * add changelog
    
    * mark theme selected
    
    * mark ssh checklist complete when ssh user is added
    
    * correct task name
    
    * use action to determine ssh setup
    
    * use a8c ssh added hook
    
    * show checklist for only newer sites
    
    * add site montioring checklist
    
    * fix plugin callback function
    
    * add filter to enable hosting checklist
    
    * do not show site title task as title is set in onboarding flow
    
    * prevent theme checklist item from being completed with initial site creation.
    
    * fix type
    
    * fix version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6673888245

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '4.16.0-alpha.1698418407',
-            'version' => '4.16.0.0-alpha1698418407',
-            'reference' => '9eda45c06b7bab7c0f29aa05cdcfc89fff0212a9',
+            'pretty_version' => '4.16.0-alpha.1698456072',
+            'version' => '4.16.0.0-alpha1698456072',
+            'reference' => '16bfb3a9b73953eaf5f35de276fd0b04f934490d',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
