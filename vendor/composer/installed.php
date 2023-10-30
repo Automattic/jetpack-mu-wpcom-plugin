7f5b76a351062cb6cc22a3e3a6f8d70154b87954
sdixon194 <sdixon194@users.noreply.github.com>
2023-10-30 21:13:55 +0000

    Revert "Backport mu-wpcom-plugin 1.7.5, Jetpack 12.8-a.9 Changes" (#33870)
    This reverts commit 44d4e860d6123153074ab574ffabee9532a9fced.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6698567892

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '4.16.0-alpha.1698456072',
-            'version' => '4.16.0.0-alpha1698456072',
-            'reference' => '16bfb3a9b73953eaf5f35de276fd0b04f934490d',
+            'pretty_version' => '4.16.0-alpha.1698699038',
+            'version' => '4.16.0.0-alpha1698699038',
+            'reference' => '3b30580c23940beb8c3e492bea4379da9f4a1ff4',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
