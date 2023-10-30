e21432482c983755d7d35c8fe65f1737c1aaa1ad
ice9js <ice9js@users.noreply.github.com>
2023-10-30 08:54:32 +0000

    Updated package versions for automattic/jetpack-mu-wpcom-plugin
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6690303863

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,9 +5,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## 1.7.5-alpha - unreleased
-
-This is an alpha version! The changes listed here are not final.
+## 1.7.5 - 2023-10-30
 
 ## 1.7.4 - 2023-10-26
 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '4.16.0-alpha.1698456072',
-            'version' => '4.16.0.0-alpha1698456072',
-            'reference' => '16bfb3a9b73953eaf5f35de276fd0b04f934490d',
+            'pretty_version' => 'v4.16.0',
+            'version' => '4.16.0.0',
+            'reference' => '8d5d899807ae681b8713755f35335d288ac4fddf',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
