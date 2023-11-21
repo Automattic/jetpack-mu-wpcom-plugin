1e9bc6e6297ba357a8e56696736b5055957ffe82
jeherve <jeherve@users.noreply.github.com>
2023-11-21 14:13:37 +0000

    Code Modernization: Replace usage of strpos() with str_contains() (#34137)
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    Co-authored-by: anomiex <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6944828534

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '5.0.1-alpha.1700575046',
-            'version' => '5.0.1.0-alpha1700575046',
-            'reference' => '927ec36ab8d395e012daeb753f1c267051c177a2',
+            'pretty_version' => '5.0.1-alpha.1700575066',
+            'version' => '5.0.1.0-alpha1700575066',
+            'reference' => 'c954a964f49bb78e84d5880c78ce6a9e40d7c5c4',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
