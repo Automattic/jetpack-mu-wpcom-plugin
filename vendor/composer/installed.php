49ebf5f65f95c94bd1c22f6e631c07601f0dc765
anomiex <anomiex@users.noreply.github.com>
2023-11-21 09:47:27 +0000

    codesniffer: Enable and fix `Modernize.FunctionCalls.Dirname.Nested` sniff (#34218)
    The WordPress-Extra ruleset turns this off. Now that we've dropped PHP
    5.6, we can turn it on.
    
    Then, of course, we have to fix all the existing instances of the
    problem.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6941880745

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-mu-wpcom' => array(
             'pretty_version' => '5.0.0',
             'version' => '5.0.0.0',
-            'reference' => '7b041508f9673b84f548e1905a6119cded8298ba',
+            'reference' => '954576bec7c7932cf68370e8d5366e60b64fdbbc',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
