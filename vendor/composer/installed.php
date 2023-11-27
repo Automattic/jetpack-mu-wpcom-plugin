5104d5ac12890b7680da7c2ddc10055dd60ea5d3
anomiex <anomiex@users.noreply.github.com>
2023-11-27 19:16:18 +0000

    Backport mu-wpcom-plugin 2.0.2, jetpack 12.9-a.11 Changes (#34298)
    * Add changelogs
    
    * Changelog edits.
    
    * Version bumps
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7009588293

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '5.1.0-alpha.1700808594',
-            'version' => '5.1.0.0-alpha1700808594',
-            'reference' => '4aafbcf87449fb5d560daffd92e75b50c323b8e4',
+            'pretty_version' => '5.1.0',
+            'version' => '5.1.0.0',
+            'reference' => '02368d8057368b2716900d9314369a6e5e8472b3',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
