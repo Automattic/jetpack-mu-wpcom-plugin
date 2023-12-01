23b048aeb483effb4c80d008c439ec13bb7861bf
yansern <yansern@users.noreply.github.com>
2023-12-01 09:25:38 +0000

    Release jetpack-mu-wpcom 5.1.1. (#34409)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7058017623

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '5.1.1-alpha.1701333262',
-            'version' => '5.1.1.0-alpha1701333262',
-            'reference' => 'd16e30c18e3097903f42984ae6e25b0fb32cbe96',
+            'pretty_version' => '5.1.1',
+            'version' => '5.1.1.0',
+            'reference' => 'cbfbee5f842333681d393e9acd587aba7833b67f',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
