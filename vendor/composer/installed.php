2ee796b0ccce4e04623e26d2ba1fa88cbf199971
simison <simison@users.noreply.github.com>
2023-12-01 10:03:37 +0000

    Launchpad: Update link for paid offer task (#34413)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7058408499

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '5.1.1',
-            'version' => '5.1.1.0',
-            'reference' => 'cbfbee5f842333681d393e9acd587aba7833b67f',
+            'pretty_version' => '5.2.0-alpha.1701424238',
+            'version' => '5.2.0.0-alpha1701424238',
+            'reference' => '71e7944722396c6f63877f5ee74c63c4ea9e8ff1',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
