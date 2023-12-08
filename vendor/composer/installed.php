df1e3d0cc45ed314d608f3338d172e63ac52891b
valterlorran <valterlorran@users.noreply.github.com>
2023-12-08 19:04:45 +0000

    Launchpad: Add the Legacy Site Setup task list (#34320)
    * Fix/css changes like avatar (#34440)
    
    * Removed like avatar border inside popup
    
    * changelog
    
    * Add title
    
    ---------
    
    Co-authored-by: Juan Lucha <juanlucha@gmail.com>
    Co-authored-by: Valter Lorran <valter.lorran@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7145137774

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '5.3.0-alpha.1702003256',
-            'version' => '5.3.0.0-alpha1702003256',
-            'reference' => '6f50a988d81788fe3c116694e8e1a301831f54bd',
+            'pretty_version' => '5.3.0-alpha.1702061469',
+            'version' => '5.3.0.0-alpha1702061469',
+            'reference' => '9b9ac4b4820cafc52e9f46cb88d7570f52f1e465',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
