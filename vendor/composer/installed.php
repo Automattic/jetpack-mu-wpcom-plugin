6282b2ef71596bb46f348d40aa6bec7c2ed6822a
taipeicoder <taipeicoder@users.noreply.github.com>
2023-12-11 09:50:20 +0000

    Add Launchpad tasks for the ai-assembler flow (#34532)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7165517032

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '5.3.0-alpha.1702061469',
-            'version' => '5.3.0.0-alpha1702061469',
-            'reference' => '9b9ac4b4820cafc52e9f46cb88d7570f52f1e465',
+            'pretty_version' => '5.3.0-alpha.1702287406',
+            'version' => '5.3.0.0-alpha1702287406',
+            'reference' => 'bc82f421f8f7120a6aaea7870d61c4e67f071e4c',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
