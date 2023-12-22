8fdf3fc7dec7d22a76a6312fb9578501a680c418
mpkelly <mpkelly@users.noreply.github.com>
2023-12-22 01:39:09 +0000

    Internal release: remove Launchpad task for domain email verification (#34387)
    * Revert #33373 (Launchpad domain email verification task)
    
    * changelog
    
    * Update versions with tools/fixup-project-versions.sh
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7295084945

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '5.5.0',
-            'version' => '5.5.0.0',
-            'reference' => 'aee984833e493712fdf67f94c39d1b0879722f72',
+            'pretty_version' => '5.6.0-alpha.1703208380',
+            'version' => '5.6.0.0-alpha1703208380',
+            'reference' => '953eeebb032bb080ae96f2df42507ffb88ba92a7',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
