333a7514d8200d1d7239f53b4f9039721298d873
valterlorran <valterlorran@users.noreply.github.com>
2024-01-10 18:36:10 +0000

    Launchpad: Add the completion logic for the `front_page_updated` task (#34837)
    * Add the completion logic for the Update your site's design task
    
    * changelog
    
    * Add the visibility check for the front page updated task
    
    * Add the visibility check to the task definition
    
    ---------
    
    Co-authored-by: Valter Lorran <valter.lorran@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7479189801

--- vendor/automattic/jetpack-mu-wpcom/CHANGELOG.md
+++ vendor/automattic/jetpack-mu-wpcom/CHANGELOG.md
@@ -9,6 +9,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 This is an alpha version! The changes listed here are not final.
 
+### Added
+- Add the completion logic for the `front_page_updated` task
+
 ### Removed
 - Removes the `Set up your Professional Email` task
 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '5.8.2-alpha.1704747252',
-            'version' => '5.8.2.0-alpha1704747252',
-            'reference' => 'dd9957c845b1aeb74e6e7a617fef62df6bcceb05',
+            'pretty_version' => '5.8.2-alpha.1704910968',
+            'version' => '5.8.2.0-alpha1704910968',
+            'reference' => '30d7bd04f783e912868d64258b18f89ad3ebb3d0',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
