c9c8eb3a37307a712af675c8b7055be3427bbc56
obenland <obenland@users.noreply.github.com>
2024-02-22 20:46:42 +0000

    Generate initial package for Scheduled Updates (#35796)
    * Generate initial package for Scheduled Updates
    
    * Fix failing GitHub actions
    
    * Run tools/fixup-project-versions.sh
    
    * Consolidate phpcs ignores
    
    * Use helper function for schedule IDs
    
    See https://github.com/Automattic/jetpack/pull/35796#discussion_r1496525266
    
    * Use the right setup method to avoid PHP version conflict
    
    See https://github.com/Automattic/jetpack/pull/35796#discussion_r1496540692
    
    * Make package work with jetpack-mu-wpcom and testing plugin
    
    * Fix composer lock file
    
    * Fixup project versions
    
    * Revert changes to mu-wpcom-plugin
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8010650662

--- vendor/automattic/jetpack-mu-wpcom/CHANGELOG.md
+++ vendor/automattic/jetpack-mu-wpcom/CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [5.14.0-alpha] - unreleased
+
+This is an alpha version! The changes listed here are not final.
+
+### Added
+- Add Scheduled Updates package
+
 ## [5.13.1] - 2024-02-21
 ### Changed
 - Ensure consistent robots.txt behavior between WP.com and WoA. [#35803]
@@ -594,6 +601,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Testing initial package release.
 
+[5.14.0-alpha]: https://github.com/Automattic/jetpack-mu-wpcom/compare/v5.13.1...v5.14.0-alpha
 [5.13.1]: https://github.com/Automattic/jetpack-mu-wpcom/compare/v5.13.0...v5.13.1
 [5.13.0]: https://github.com/Automattic/jetpack-mu-wpcom/compare/v5.12.2...v5.13.0
 [5.12.2]: https://github.com/Automattic/jetpack-mu-wpcom/compare/v5.12.1...v5.12.2
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitd9d132a783958a00a2c7cccff60ca42d_jetpack_mu_wpcom_pluginⓥ2_0_23
+class ComposerStaticInitd9d132a783958a00a2c7cccff60ca42d_jetpack_mu_wpcom_pluginⓥ2_0_24_alpha
 {
     public static $files = array (
         '3773ef3f09c37da5478d578e32b03a4b' => __DIR__ . '/..' . '/automattic/jetpack-assets/actions.php',
@@ -15,6 +15,7 @@ class ComposerStaticInitd9d132a783958a00a2c7cccff60ca42d_jetpack_mu_wpcom_plugin
         'Automattic\\Jetpack\\Assets\\Semver' => __DIR__ . '/..' . '/automattic/jetpack-assets/src/class-semver.php',
         'Automattic\\Jetpack\\Constants' => __DIR__ . '/..' . '/automattic/jetpack-constants/src/class-constants.php',
         'Automattic\\Jetpack\\Jetpack_Mu_Wpcom' => __DIR__ . '/..' . '/automattic/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php',
+        'Automattic\\Jetpack\\Scheduled_Updates' => __DIR__ . '/..' . '/automattic/scheduled-updates/src/class-scheduled-updates.php',
         'Automattic\\Jetpack\\Verbum_Comments' => __DIR__ . '/..' . '/automattic/jetpack-mu-wpcom/src/features/verbum-comments/class-verbum-comments.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'Launchpad_Task_Lists' => __DIR__ . '/..' . '/automattic/jetpack-mu-wpcom/src/features/launchpad/class-launchpad-task-lists.php',
@@ -23,6 +24,7 @@ class ComposerStaticInitd9d132a783958a00a2c7cccff60ca42d_jetpack_mu_wpcom_plugin
         'Verbum_Gutenberg_Editor' => __DIR__ . '/..' . '/automattic/jetpack-mu-wpcom/src/features/verbum-comments/assets/class-verbum-gutenberg-editor.php',
         'WPCOM_REST_API_V2_Endpoint_Launchpad' => __DIR__ . '/..' . '/automattic/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad.php',
         'WPCOM_REST_API_V2_Endpoint_Launchpad_Navigator' => __DIR__ . '/..' . '/automattic/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad-navigator.php',
+        'WPCOM_REST_API_V2_Endpoint_Update_Schedules' => __DIR__ . '/..' . '/automattic/scheduled-updates/src/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-update-schedules.php',
         'WPCOM_REST_API_V2_Verbum_Auth' => __DIR__ . '/..' . '/automattic/jetpack-mu-wpcom/src/features/verbum-comments/assets/class-wpcom-rest-api-v2-verbum-auth.php',
         'WPCOM_REST_API_V2_Verbum_OEmbed' => __DIR__ . '/..' . '/automattic/jetpack-mu-wpcom/src/features/verbum-comments/assets/class-wpcom-rest-api-v2-verbum-oembed.php',
         'Wpcom_Block_Patterns_From_Api' => __DIR__ . '/..' . '/automattic/jetpack-mu-wpcom/src/features/block-patterns/class-wpcom-block-patterns-from-api.php',
@@ -32,7 +34,7 @@ class ComposerStaticInitd9d132a783958a00a2c7cccff60ca42d_jetpack_mu_wpcom_plugin
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->classMap = ComposerStaticInitd9d132a783958a00a2c7cccff60ca42d_jetpack_mu_wpcom_pluginⓥ2_0_23::$classMap;
+            $loader->classMap = ComposerStaticInitd9d132a783958a00a2c7cccff60ca42d_jetpack_mu_wpcom_pluginⓥ2_0_24_alpha::$classMap;
 
         }, null, ClassLoader::class);
     }
