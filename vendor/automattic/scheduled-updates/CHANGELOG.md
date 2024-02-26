2e876a880d17a93179a8b7ff43edb3ac3aaa83fb
obenland <obenland@users.noreply.github.com>
2024-02-26 16:26:46 +0000

    Scheduled Updates: Make schedule execution work (#35885)
    * Fix schedule args to be one array of plugins
    
    * Make authenticated request
    
    * Revert "Fix schedule args to be one array of plugins"
    
    This reverts commit db00f86cbb39f099572ad80775a720dba0e8020a.
    
    * Capture all plugins that are being passed
    
    * Revert change in parameter docs
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8052031941

--- vendor/automattic/scheduled-updates/CHANGELOG.md
+++ vendor/automattic/scheduled-updates/CHANGELOG.md
@@ -12,3 +12,6 @@ This is an alpha version! The changes listed here are not final.
 ### Added
 - Generate initial package for Scheduled Updates
 - Show schedule information for scheduled plugin updates in wp-admin
+
+### Changed
+- Fix up cron callback and schedule generation to make schedule execution work
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '2.1.1',
             'version' => '2.1.1.0',
-            'reference' => 'c3f2e29cf5ed33bc27a08f35f8fd1ac0a9fbdb94',
+            'reference' => '5bdd27169ae01ffe388cd6e52b55d0ca05e91a01',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.0',
             'version' => '2.0.0.0',
-            'reference' => '635a8ed6a936422d9fd858c4166e0d43311121c2',
+            'reference' => 'fa1849bd2656aeeef38466ff233fc40f22338ee5',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-constants',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-mu-wpcom' => array(
             'pretty_version' => '5.14.0-alpha.1708940746',
             'version' => '5.14.0.0-alpha1708940746',
-            'reference' => '7426f5f2258bd0da6c3b85b280f0e2fca888f5c9',
+            'reference' => '08fd730b954fbaaa64b27eac8e640f254b6a2adf',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
@@ -47,9 +47,9 @@
             'dev_requirement' => false,
         ),
         'automattic/scheduled-updates' => array(
-            'pretty_version' => '0.1.0-alpha.1708961343',
-            'version' => '0.1.0.0-alpha1708961343',
-            'reference' => '69d5e79371310e443effec330568b16bebdb8021',
+            'pretty_version' => '0.1.0-alpha.1708963962',
+            'version' => '0.1.0.0-alpha1708963962',
+            'reference' => '235209bcd7b3ef203a99476bdb74563e7c107604',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/scheduled-updates',
             'aliases' => array(),
