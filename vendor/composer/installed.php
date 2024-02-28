2ae45b0b3057ca840c9c02eaf35b3fddce15e137
jeherve <jeherve@users.noreply.github.com>
2024-02-28 14:37:47 +0000

    Host utilities: bail early when dns_get_record is not defined. (#36019)
    * Host utilities: bail early when dns_get_record is not defined.
    
    See p1HpG7-rqu-p2
    
    * Address review feedback
    
    - https://github.com/Automattic/jetpack/pull/36019#discussion_r1505734224
    - https://github.com/Automattic/jetpack/pull/36019#discussion_r1505736311
    Co-authored-by: Juanma Rodriguez Escriche <juanma.rodriguez.escriche@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8081936791

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '2.1.1',
             'version' => '2.1.1.0',
-            'reference' => 'f63f7cf97d6b56d23df2c75870b8b1aa7cebd450',
+            'reference' => 'c7e04633a03119053f25bd5cb4dc1ef6305105d6',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.0',
             'version' => '2.0.0.0',
-            'reference' => '75c758324429b448840a3afb6cbdad60f107c6ce',
+            'reference' => '6d43d77482aaa97327d4a87ccd073900629c188e',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-constants',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-mu-wpcom' => array(
             'pretty_version' => '5.14.2-alpha.1709116066',
             'version' => '5.14.2.0-alpha1709116066',
-            'reference' => 'c5966a279a36c1c79a3091da5a67e9833529d20a',
+            'reference' => '6420e557d13126a76e4f6a1dc898aa3121d0cff9',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
@@ -49,7 +49,7 @@
         'automattic/scheduled-updates' => array(
             'pretty_version' => '0.2.1-alpha.1709105648',
             'version' => '0.2.1.0-alpha1709105648',
-            'reference' => 'b1d6d8041ea23b7c1e39a2a4456529a621cef097',
+            'reference' => 'd6d067e5550f28acb7f53250e61f7d2e071d4da2',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/scheduled-updates',
             'aliases' => array(),
