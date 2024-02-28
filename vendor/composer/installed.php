66ca2d805e5102ad635f45b85a59dc33715d60b1
jeherve <jeherve@users.noreply.github.com>
2024-02-28 08:11:14 +0000

    Connection: avoid deprecation warning during authorization (#35996)
    This should fix warnings like this one:
    
    ```
    PHP Deprecated:  Creation of dynamic property Automattic\Jetpack\Connection\Webhooks\Authorize_Redirect::$connection is deprecated in /usr/local/src/jetpack-monorepo/projects/packages/connection/src/webhooks/class-authorize-redirect.php on line 28
    ```
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8077059159

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '2.1.1',
             'version' => '2.1.1.0',
-            'reference' => 'f3df5ccf43e8804804117027195f38412e19fef6',
+            'reference' => '6a253893d2de8d749033aff6ee696f016421670a',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.0',
             'version' => '2.0.0.0',
-            'reference' => 'ce44a37c39d3fe027e1dd930052109795ebc2ff4',
+            'reference' => '532a41d4014b1bcb75935e21c8340931f28cf493',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-constants',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-mu-wpcom' => array(
             'pretty_version' => '5.14.2-alpha.1709105648',
             'version' => '5.14.2.0-alpha1709105648',
-            'reference' => '6dfe1699fed02402c8f3f1063300ba95b628a1ac',
+            'reference' => 'f54de807d15c785e59ee42f45b8937bdd3a75e46',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
@@ -49,7 +49,7 @@
         'automattic/scheduled-updates' => array(
             'pretty_version' => '0.2.1-alpha.1709105648',
             'version' => '0.2.1.0-alpha1709105648',
-            'reference' => 'a67599a24bde9f74f3605a9828c9cfe8121183e3',
+            'reference' => '954f5bf0dc705125d72db416d6ca710d35772143',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/scheduled-updates',
             'aliases' => array(),
