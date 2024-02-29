e6c6ddd1bb12f4902d267898fe09aad2522d0cfd
ouikhuan <ouikhuan@users.noreply.github.com>
2024-02-29 09:59:58 +0000

    Scheduled Updates: Add scheduled update body parameter (#35941)
    * Add a new is_scheduled_updates param to the modify plugin endpoint
    
    * Add conditions for scheduled update only
    
    * pass is_scheduled_updates property in the allowlist filter
    
    * changelog
    
    * Replace slug property with plugin property
    
    * Identify scheduled update by constant
    
    * Add new constant SCHEDULED_UPDATE to identify requests
    
    * remove unnecessary check
    
    * Prevent making changes to core structure
    
    * Change scheduled updates to singular
    
    * Change the wording to make unauthorized message more clearer
    
    * Update changelog with more clearer information
    
    * Change the function name in changelog to align with latest version
    
    * Fix version mismatch
    
    * Remove unnecessary check
    
    * Remove unnecessary check
    
    * Reformat changelog entry
    
    * Remove blank lines
    
    * Rename variale name
    
    * set lock release timeout if user is from scheduled update
    
    * Fix version mismatch
    
    * Fix typo in body param
    
    See D140023-code
    
    * Add missing typo fix
    
    ---------
    
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8094040675

--- vendor/automattic/scheduled-updates/CHANGELOG.md
+++ vendor/automattic/scheduled-updates/CHANGELOG.md
@@ -5,13 +5,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.2.2-alpha] - unreleased
+## [0.3.0-alpha] - unreleased
 
 This is an alpha version! The changes listed here are not final.
 
 ### Changed
 - Aligned handling of schedules that can't be found to return the same error messages.
 - Scheduled Updates: Load API endpoints on WP.com so it works with public-api passthrough.
+- Scheduled updates: Modified the `allowlist_scheduled_plugins` function to check scheduled update requests.
+  
+  Change the `allowlist_scheduled_plugins` function to include a check for the `SCHEDULED_AUTOUPDATE` constant. This allows us to identify requests coming from scheduled updates and include the relevant plugins when the `auto_update_plugin` hook is triggered.
 
 ## [0.2.1] - 2024-02-27
 ### Changed
@@ -28,6 +31,6 @@ This is an alpha version! The changes listed here are not final.
 ### Added
 - Generate initial package for Scheduled Updates [#35796]
 
-[0.2.2-alpha]: https://github.com/Automattic/scheduled-updates/compare/v0.2.1...v0.2.2-alpha
+[0.3.0-alpha]: https://github.com/Automattic/scheduled-updates/compare/v0.2.1...v0.3.0-alpha
 [0.2.1]: https://github.com/Automattic/scheduled-updates/compare/v0.2.0...v0.2.1
 [0.2.0]: https://github.com/Automattic/scheduled-updates/compare/v0.1.0...v0.2.0
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '2.1.1',
             'version' => '2.1.1.0',
-            'reference' => '65cb6dd3a9708217dd1371f3ca5ab194db8256dd',
+            'reference' => '40417e99691310bfd38235fb3f2662273de91d75',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.0',
             'version' => '2.0.0.0',
-            'reference' => '24bc5dca8ac86ce1b2df7394c63d7c5b5f1609b4',
+            'reference' => '13cd3c8d20882e7c91f738643b7d157d14b557ab',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-constants',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-mu-wpcom' => array(
             'pretty_version' => '5.14.2-alpha.1709130294',
             'version' => '5.14.2.0-alpha1709130294',
-            'reference' => 'a6978243418452284e022f1c050af1fc70734f20',
+            'reference' => '2e9bce053f46bb16a3ed134008893c91ccd9fd0a',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
@@ -47,9 +47,9 @@
             'dev_requirement' => false,
         ),
         'automattic/scheduled-updates' => array(
-            'pretty_version' => '0.2.2-alpha.1709154910',
-            'version' => '0.2.2.0-alpha1709154910',
-            'reference' => '75e1d3da940bced8219f48a32c6add2a16030667',
+            'pretty_version' => '0.3.0-alpha.1709200063',
+            'version' => '0.3.0.0-alpha1709200063',
+            'reference' => '0f55a7be3333f2d1d3d45b96797ea30d3289a9ba',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/scheduled-updates',
             'aliases' => array(),
