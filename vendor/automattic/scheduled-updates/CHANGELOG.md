0af6daf12d73a39d55fee00f7710d9ed9dddbcaa
zinigor <zinigor@users.noreply.github.com>
2024-03-18 19:08:04 +0000

    Backport Jetpack 13.3-a.3 Jetpack-mu-plugin 2.1.6 Changes (#36439)
    * Changelog and readme.txt edits.
    
    * Init new cycle.
    
    * Version bumps.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8332113517

--- vendor/automattic/scheduled-updates/CHANGELOG.md
+++ vendor/automattic/scheduled-updates/CHANGELOG.md
@@ -5,9 +5,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.4.1-alpha] - unreleased
-
-This is an alpha version! The changes listed here are not final.
+## [0.4.1] - 2024-03-18
+### Changed
+- Internal updates.
 
 ## [0.4.0] - 2024-03-15
 ### Added
@@ -74,7 +74,7 @@ This is an alpha version! The changes listed here are not final.
 ### Added
 - Generate initial package for Scheduled Updates [#35796]
 
-[0.4.1-alpha]: https://github.com/Automattic/scheduled-updates/compare/v0.4.0...v0.4.1-alpha
+[0.4.1]: https://github.com/Automattic/scheduled-updates/compare/v0.4.0...v0.4.1
 [0.4.0]: https://github.com/Automattic/scheduled-updates/compare/v0.3.4...v0.4.0
 [0.3.4]: https://github.com/Automattic/scheduled-updates/compare/v0.3.3...v0.3.4
 [0.3.3]: https://github.com/Automattic/scheduled-updates/compare/v0.3.2...v0.3.3
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-assets' => array(
-            'pretty_version' => '2.1.5-alpha.1710536348',
-            'version' => '2.1.5.0-alpha1710536348',
-            'reference' => 'b38447cce4abe25a90649f12ee9600ad1b8be959',
+            'pretty_version' => '2.1.5',
+            'version' => '2.1.5.0',
+            'reference' => '2e2103bc5e3d372128dbbd4f58f89fe7e21e9d3c',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.1',
             'version' => '2.0.1.0',
-            'reference' => '1003bc2790660ee0f4413c4f99212f0ed712971f',
+            'reference' => '1fb924b571c98d4ec6a34ce0b0f9c01c15a6a353',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-constants',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-mu-wpcom' => array(
             'pretty_version' => '5.17.0',
             'version' => '5.17.0.0',
-            'reference' => '4ede13305614680c6b62bb0286dabaa4cbf2824b',
+            'reference' => '8ebf1fda8a21acd9a57459fa4d3386d837ac1f6c',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
@@ -47,9 +47,9 @@
             'dev_requirement' => false,
         ),
         'automattic/scheduled-updates' => array(
-            'pretty_version' => '0.4.1-alpha.1710525888',
-            'version' => '0.4.1.0-alpha1710525888',
-            'reference' => '66d909e0226b4c5545e4b403aa36757c32d89e36',
+            'pretty_version' => '0.4.1',
+            'version' => '0.4.1.0',
+            'reference' => '8dd7cff2beca36c4d19980be64b744eeb29c2856',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/scheduled-updates',
             'aliases' => array(),
