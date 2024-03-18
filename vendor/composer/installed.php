a37a1f5e34ca60ab4986ad25895c7a237b0d3181
zinigor <zinigor@users.noreply.github.com>
2024-03-18 17:27:29 +0000

    Changelog and readme.txt edits.
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8330908410

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-assets' => array(
-            'pretty_version' => '2.1.5-alpha.1710536348',
-            'version' => '2.1.5.0-alpha1710536348',
-            'reference' => 'd0676bec5937d5bc349f8f785b10db4db388b5d8',
+            'pretty_version' => '2.1.5',
+            'version' => '2.1.5.0',
+            'reference' => '244ac0bd3c27dd5bff0a5e2b8af9cc722d021b7c',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.1',
             'version' => '2.0.1.0',
-            'reference' => '6a27689f2b4ab4f63c157ebe2baa7d635a0feda9',
+            'reference' => '893e5d4ec805e350b5d930cb0d81df44fd06f85c',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-constants',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-mu-wpcom' => array(
             'pretty_version' => '5.17.0',
             'version' => '5.17.0.0',
-            'reference' => '9faeaf35e06cc20a4ecef178f0f728f849f4519a',
+            'reference' => 'd6e571f098f2ae37c26458a195798e5bbb840880',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
@@ -47,9 +47,9 @@
             'dev_requirement' => false,
         ),
         'automattic/scheduled-updates' => array(
-            'pretty_version' => '0.4.1-alpha.1710525888',
-            'version' => '0.4.1.0-alpha1710525888',
-            'reference' => '3a4a540e2d4811b235ca9bd95b381cd3be7e0af2',
+            'pretty_version' => '0.4.1',
+            'version' => '0.4.1.0',
+            'reference' => '9e9b91ce4238c7926ee68be71e34468b793f6831',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/scheduled-updates',
             'aliases' => array(),
