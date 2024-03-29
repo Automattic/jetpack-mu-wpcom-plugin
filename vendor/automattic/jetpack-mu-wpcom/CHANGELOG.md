699ae65c500fece383886794accced64f98bfd11
miksansegundo <miksansegundo@users.noreply.github.com>
2024-03-29 16:25:09 +0000

    Dotcom patterns: Show v2 pattern library with all themes (#36588)
    * Fetch wp_block post type patterns
    
    * changelog
    
    * Fix tests
    
    * Fix test
    
    * Fix version
    
    * Improve cache and remove source site array
    
    * Fix version
    
    * Fix tests
    
    * Fix test
    
    * Remove redundant cast
    
    * Hide core and Jetpack form patterns
    
    * Update changelog
    
    * Update changelog
    
    * Add is_automattician as a phan stub
    
    * Add param to stub
    
    * More specific phpcs:ignore
    
    * Update Phan baselines
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8483007792

--- vendor/automattic/jetpack-mu-wpcom/CHANGELOG.md
+++ vendor/automattic/jetpack-mu-wpcom/CHANGELOG.md
@@ -5,7 +5,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [5.21.1-alpha] - unreleased
+## [5.22.0-alpha] - unreleased
 
 This is an alpha version! The changes listed here are not final.
 
@@ -13,6 +13,7 @@ This is an alpha version! The changes listed here are not final.
 - Change Phan baselines.
 
 ### Changed
+- Dotcom patterns: use wp_block post type patterns in editor with all themes and hide core and Jetpack form patterns
 - General: update Phan configuration.
 
 ## [5.21.0] - 2024-03-27
@@ -697,7 +698,7 @@ This is an alpha version! The changes listed here are not final.
 
 - Testing initial package release.
 
-[5.21.1-alpha]: https://github.com/Automattic/jetpack-mu-wpcom/compare/v5.21.0...v5.21.1-alpha
+[5.22.0-alpha]: https://github.com/Automattic/jetpack-mu-wpcom/compare/v5.21.0...v5.22.0-alpha
 [5.21.0]: https://github.com/Automattic/jetpack-mu-wpcom/compare/v5.20.0...v5.21.0
 [5.20.0]: https://github.com/Automattic/jetpack-mu-wpcom/compare/v5.19.0...v5.20.0
 [5.19.0]: https://github.com/Automattic/jetpack-mu-wpcom/compare/v5.18.0...v5.19.0
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '2.1.6',
             'version' => '2.1.6.0',
-            'reference' => '9a8968f5e48caf66910d17166c33ba843e7e04d1',
+            'reference' => 'f30fd8d336c59c6f63ee657e042070fd61687891',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,16 +22,16 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.1',
             'version' => '2.0.1.0',
-            'reference' => '3630f731713766a4e453fa67c3df94dc42f710b3',
+            'reference' => 'd9c974b6bc710a3aebd0eb5acda8d539f301be3b',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-constants',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '5.21.1-alpha.1711693124',
-            'version' => '5.21.1.0-alpha1711693124',
-            'reference' => '861a3cabd10cba7c9d88382201882ef3a9b111a7',
+            'pretty_version' => '5.22.0-alpha.1711728682',
+            'version' => '5.22.0.0-alpha1711728682',
+            'reference' => '8bc8fec935547e50b7a964ba0c1b1d4ff55fa6f9',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
@@ -49,7 +49,7 @@
         'automattic/scheduled-updates' => array(
             'pretty_version' => '0.5.3-alpha.1711633255',
             'version' => '0.5.3.0-alpha1711633255',
-            'reference' => '6c45d73c004ff260eaa4203818295e0420df7b4a',
+            'reference' => 'c19a7cf28c958ec863a31fb07a659f3d3cf9e25d',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/scheduled-updates',
             'aliases' => array(),
