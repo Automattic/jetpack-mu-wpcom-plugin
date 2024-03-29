6222337f3a33dcaae936fb5b96378f2e2b25b508
taipeicoder <taipeicoder@users.noreply.github.com>
2024-03-29 06:31:16 +0000

    Phan: Add stub for wordpress functions (#36627)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8477961979

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '2.1.6',
             'version' => '2.1.6.0',
-            'reference' => '853796d3eb6b54908aad89888aa1680d69e423b9',
+            'reference' => '9a8968f5e48caf66910d17166c33ba843e7e04d1',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,16 +22,16 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.1',
             'version' => '2.0.1.0',
-            'reference' => '481ea677e11d8bc3edd8568748251f3a5f715225',
+            'reference' => '3630f731713766a4e453fa67c3df94dc42f710b3',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-constants',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'automattic/jetpack-mu-wpcom' => array(
-            'pretty_version' => '5.21.1-alpha.1711633255',
-            'version' => '5.21.1.0-alpha1711633255',
-            'reference' => 'd4de69f6585e7d7d217857f96535eb8d28094595',
+            'pretty_version' => '5.21.1-alpha.1711693124',
+            'version' => '5.21.1.0-alpha1711693124',
+            'reference' => '861a3cabd10cba7c9d88382201882ef3a9b111a7',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-mu-wpcom',
             'aliases' => array(),
@@ -49,7 +49,7 @@
         'automattic/scheduled-updates' => array(
             'pretty_version' => '0.5.3-alpha.1711633255',
             'version' => '0.5.3.0-alpha1711633255',
-            'reference' => '551b5773190c60d5ba61b8f1c8033c0025fdaabf',
+            'reference' => '6c45d73c004ff260eaa4203818295e0420df7b4a',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/scheduled-updates',
             'aliases' => array(),
