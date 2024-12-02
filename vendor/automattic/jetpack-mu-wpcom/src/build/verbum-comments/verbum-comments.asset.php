81879ed16e81f66620b624c4d772aeb80613f234
anomiex <anomiex@users.noreply.github.com>
2024-12-02 21:33:02 +0000

    Update dependency @preact/signals to v1.3.1 (#40403)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12128141511
    
    Upstream-Ref: Automattic/jetpack@bff6b009e71d173d590653c34889619b16d5c2c1

--- vendor/automattic/jetpack-mu-wpcom/src/build/verbum-comments/verbum-comments.asset.php
+++ vendor/automattic/jetpack-mu-wpcom/src/build/verbum-comments/verbum-comments.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '97043884103359931e09');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '32c1e4fa34d4052cef07');
