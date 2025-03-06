6976cc35769f1d5e361ccb6e3039269bd528ff3c
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-03-06 04:43:30 +0000

    Social | Move the editor components from Social/Jetpack to publicize-components (#41836)
    * Move useUpgradeFlow hook to shared extension utils package
    
    * Move components from Jetpack and organize them a bit
    
    * Prepare the entry points for Jetpack and Social plugins
    
    * Fix the TS errors
    
    * Prepare the webpack entrypoints in publicize package
    
    * Enqueue the new assets from publicize package
    
    * Remove the unused blocks from Jetpack plugin
    
    * Fix specificity for broken connection link
    
    * Remove the unused code from Social plugin
    
    * Nuke all things JS in Social
    
    * Remove assets registration from Social
    
    * Fix siteFragment error in the stale initial state
    
    * Add changelogs
    
    * Restore publicize block PHP logic for WPCOM sites
    
    * We need to load the editor assets even if publicize is OFF
    
    * Do not render the panels if publicize is OFF
    
    * Fix the script data path
    
    * Restore "concurrently" dependency
    
    * Enqueue block assets only if the user has the capability
    
    * Fix compatibility with old plugin versions
    
    * Update the version following the Jetpack release
    
    * The trunk version is still 14.4
    
    * Wire up jetpackSidebarIsOpen for social sidebar
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13691203645
    
    Upstream-Ref: Automattic/jetpack@f804196908e89e6589094a035c8c1437a43de7f3

--- jetpack_vendor/automattic/jetpack-mu-wpcom/src/build/tags-education/tags-education.asset.php
+++ jetpack_vendor/automattic/jetpack-mu-wpcom/src/build/tags-education/tags-education.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'c86a7cb851b5740c4bae');
+<?php return array('dependencies' => array('jetpack-connection', 'lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => '8ca56ad456486b9887ba');
