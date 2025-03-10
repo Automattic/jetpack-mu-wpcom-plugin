e07a2a7112afdac7c2647fc3f928031a51907023
tbradsha <tbradsha@users.noreply.github.com>
2025-03-10 13:18:49 +0000

    Lock file maintenance (#42316)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13765310170
    
    Upstream-Ref: Automattic/jetpack@c11f962f7de0bce2e4a6f780e7b7b05934f99b58

--- jetpack_vendor/automattic/jetpack-mu-wpcom/src/build/wpcom-dashboard-widgets/wpcom-dashboard-widgets.asset.php
+++ jetpack_vendor/automattic/jetpack-mu-wpcom/src/build/wpcom-dashboard-widgets/wpcom-dashboard-widgets.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '41622b8a1e55ca88e46b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b8c6daa5d65697b8cffd');
