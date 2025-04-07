5f65b405b2fd7d291fb1fe4449ad818a45f7d626
anomiex <anomiex@users.noreply.github.com>
2025-04-07 16:37:52 +0000

    Lock file maintenance (#42932)
    * Lock file maintenance
    * Downgrade `@automattic/data-stores`
    * Apply pnpmfile hack for framer-motion to @wordpress/dataviews dep too
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14314336022
    
    Upstream-Ref: Automattic/jetpack@3c9c5d2274eb7c861f1998eaeb0f6d3bc927be2c

--- jetpack_vendor/automattic/jetpack-mu-wpcom/src/build/wpcom-media-url-upload/wpcom-media-url-upload.asset.php
+++ jetpack_vendor/automattic/jetpack-mu-wpcom/src/build/wpcom-media-url-upload/wpcom-media-url-upload.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n', 'wp-polyfill'), 'version' => 'c5ee54da8efeab880fcc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'c5ee54da8efeab880fcc');
--- jetpack_vendor/automattic/jetpack-mu-wpcom/src/build/wpcom-replace-site-visibility/wpcom-replace-site-visibility.asset.php
+++ jetpack_vendor/automattic/jetpack-mu-wpcom/src/build/wpcom-replace-site-visibility/wpcom-replace-site-visibility.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'b2c7b09603290d3c31dd');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-i18n', 'wp-url'), 'version' => 'b2c7b09603290d3c31dd');
