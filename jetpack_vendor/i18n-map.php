cbb762af78829b7f3e5a9d9a76980941e154f247
anomiex <anomiex@users.noreply.github.com>
2025-04-22 20:24:28 +0000

    phpunit: Enable reporting and failing on PHPUnit 11 deprecations (#43179)
    Now that we've fixed all the PHPUnit 11 deprecations, adjust the configs
    to enable `displayDetailsOnPhpunitDeprecations` and
    `failOnPhpunitDeprecation`.
    
    For plugins/crm, plugins/jetpack, and plugins/wpcomsh, there's one more
    deprecation left that's also fixed in this PR: WP Core's obsolete
    `WP_UnitTestCase_Base::checkRequirements()` function has a doc comment
    that PHPUnit is mis-parsing to see a `@group` annotation that's not
    actually intended. We can override it to stop PHPUnit from seeing that
    doc comment.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14603629889
    
    Upstream-Ref: Automattic/jetpack@c78f78dae7d54c7cd98b5b189bd4c4485fb100c5

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,15 +6,15 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.8-alpha1745351100',
+      'ver' => '0.5.8-alpha1745352378',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.19-alpha1745351100',
+      'ver' => '4.0.19-alpha1745352378',
     ),
     'jetpack-blaze' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-blaze',
-      'ver' => '0.25.22-alpha1745351100',
+      'ver' => '0.25.22-alpha1745352378',
     ),
     'jetpack-calypsoify' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-calypsoify',
@@ -30,51 +30,51 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.11.1-alpha1745351100',
+      'ver' => '6.11.1-alpha1745352378',
     ),
     'jetpack-google-analytics' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-google-analytics',
-      'ver' => '0.3.1',
+      'ver' => '0.3.2-alpha1745352378',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.9-alpha1745351100',
+      'ver' => '0.4.9-alpha1745352378',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.14-alpha1745351100',
+      'ver' => '4.2.14-alpha1745352378',
     ),
     'jetpack-masterbar' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-masterbar',
-      'ver' => '0.17.0-alpha1745351100',
+      'ver' => '0.17.0-alpha1745352378',
     ),
     'jetpack-mu-wpcom' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-mu-wpcom',
-      'ver' => '6.4.0-alpha1745351100',
+      'ver' => '6.4.0-alpha1745352378',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.8-alpha1745351100',
+      'ver' => '0.4.8-alpha1745352378',
     ),
     'jetpack-scheduled-updates' => array(
       'path' => 'jetpack_vendor/automattic/scheduled-updates',
-      'ver' => '0.14.2-alpha1745351100',
+      'ver' => '0.14.2-alpha1745352378',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
-      'ver' => '0.16.2-alpha1745351100',
+      'ver' => '0.16.2-alpha1745352378',
     ),
     'jetpack-stats-admin' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats-admin',
-      'ver' => '0.25.2',
+      'ver' => '0.25.3-alpha1745352378',
     ),
     'jetpack-subscribers-dashboard' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-subscribers-dashboard',
-      'ver' => '0.1.8-alpha1745351100',
+      'ver' => '0.1.8-alpha1745352378',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.11.1-alpha1745351100',
+      'ver' => '4.11.1-alpha1745352378',
     ),
   ),
 );
