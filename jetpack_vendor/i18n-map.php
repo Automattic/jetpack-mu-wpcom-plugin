97ace4c1ac6f419733b991b78adf9cb35d9870ff
jeherve <jeherve@users.noreply.github.com>
2025-06-23 09:21:02 +0000

    Sharing buttons blocks: add ability to count sharing events (#44039)
    * Sharing buttons blocks: add ability to count sharing events
    
    This is only useful and necessary for WordPress.com simple, and avoids issues when counting shares there.
    
    The contents of the methods are directly copied from the existing method for the legacy sharing buttons, in Sharing_Source. I only added the phpcs:ignore.
    
    See pejTkB-25H-p2
    
    * Cast int as string to avoid errors
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15819983013
    
    Upstream-Ref: Automattic/jetpack@f6148e8f502634dd812965220f7b79cc885d1380

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.28-alpha1750347518',
+      'ver' => '4.0.28',
     ),
     'jetpack-blaze' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-blaze',
-      'ver' => '0.25.31-alpha1750347518',
+      'ver' => '0.25.31',
     ),
     'jetpack-calypsoify' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-calypsoify',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.13.7-alpha1750347518',
+      'ver' => '6.13.7',
     ),
     'jetpack-google-analytics' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-google-analytics',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.25-alpha1750347518',
+      'ver' => '4.2.25',
     ),
     'jetpack-masterbar' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-masterbar',
@@ -62,11 +62,11 @@ return array(
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
-      'ver' => '0.17.0-alpha1750652294',
+      'ver' => '0.17.0',
     ),
     'jetpack-stats-admin' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats-admin',
-      'ver' => '0.27.0-alpha1750652294',
+      'ver' => '0.27.0',
     ),
     'jetpack-subscribers-dashboard' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-subscribers-dashboard',
