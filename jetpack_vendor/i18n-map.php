f0cce455d2ef45cc88e00c4dc8d5c3c22b3de369
ArSn <ArSn@users.noreply.github.com>
2025-07-03 14:27:46 +0000

    Backport protect 4.2.0 Changes (#44180)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16052741591
    
    Upstream-Ref: Automattic/jetpack@e7af63a819f8d7b214bc6900a997caea94cd3798

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.32-alpha1751381215',
+      'ver' => '4.0.32',
     ),
     'jetpack-blaze' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-blaze',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.13.13-alpha1751381215',
+      'ver' => '6.13.13',
     ),
     'jetpack-google-analytics' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-google-analytics',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.27-alpha1751382703',
+      'ver' => '4.2.27',
     ),
     'jetpack-masterbar' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-masterbar',
