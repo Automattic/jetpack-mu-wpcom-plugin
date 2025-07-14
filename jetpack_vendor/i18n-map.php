ed4cd3ad75326fc436823080ad4d9cf1792f7f5c
tbradsha <tbradsha@users.noreply.github.com>
2025-07-14 18:35:29 +0000

    Backport jetpack 14.9-a.3 Changes (#44300)
    * Changelog and readme.txt edits.
    
    * Delete placeholder test
    
    * Set failOnEmptyTestSuite to false
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16274631552
    
    Upstream-Ref: Automattic/jetpack@c0119ef850ee366fb740c930d058a0052bb0a59d

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,7 +14,7 @@ return array(
     ),
     'jetpack-blaze' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-blaze',
-      'ver' => '0.25.36-alpha1751983262',
+      'ver' => '0.25.36',
     ),
     'jetpack-calypsoify' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-calypsoify',
@@ -22,7 +22,7 @@ return array(
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.13.10-alpha1751983262',
+      'ver' => '0.13.10',
     ),
     'jetpack-compat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-compat',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.14.2-alpha1752171648',
+      'ver' => '6.14.2',
     ),
     'jetpack-google-analytics' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-google-analytics',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-masterbar' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-masterbar',
-      'ver' => '0.18.2-alpha1752223264',
+      'ver' => '0.18.2',
     ),
     'jetpack-mu-wpcom' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-mu-wpcom',
@@ -66,11 +66,11 @@ return array(
     ),
     'jetpack-stats-admin' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats-admin',
-      'ver' => '0.28.0-alpha1752066464',
+      'ver' => '0.28.0',
     ),
     'jetpack-subscribers-dashboard' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-subscribers-dashboard',
-      'ver' => '0.1.22-alpha1752071309',
+      'ver' => '0.1.22',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
