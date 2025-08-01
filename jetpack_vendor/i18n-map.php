666e518549f6c3619fb0b194fd44d60f181e9e9f
BogdanUngureanu <BogdanUngureanu@users.noreply.github.com>
2025-08-01 12:59:41 +0000

    Untangling: Remove Account settings and Site Settings menu items (#44570)
    * Untangling: Remove Account settings and Site Settings menu items
    
    * fix tests
    
    * fix failing test
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16675390218
    
    Upstream-Ref: Automattic/jetpack@61bd53e0eac169b1d5456ad010e1322ada7b3fe5

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -46,11 +46,11 @@ return array(
     ),
     'jetpack-masterbar' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-masterbar',
-      'ver' => '0.19.2-alpha1753906593',
+      'ver' => '0.19.2-alpha1754052250',
     ),
     'jetpack-mu-wpcom' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-mu-wpcom',
-      'ver' => '6.7.0-alpha1753906593',
+      'ver' => '6.7.0-alpha1754052250',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
