4c80871ae9f8a4f56a46c23c1e6bf54bfb16c723
simison <simison@users.noreply.github.com>
2025-08-01 12:23:27 +0000

    Update @wordpress/dataviews from v4.17.0 → v5.0.0 (#44376)
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16674618737
    
    Upstream-Ref: Automattic/jetpack@828c113f9b7872359d714fd3565cf95c402b1c39

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-subscribers-dashboard' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-subscribers-dashboard',
-      'ver' => '0.1.25-alpha1753906593',
+      'ver' => '0.2.0-alpha1754049993',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
