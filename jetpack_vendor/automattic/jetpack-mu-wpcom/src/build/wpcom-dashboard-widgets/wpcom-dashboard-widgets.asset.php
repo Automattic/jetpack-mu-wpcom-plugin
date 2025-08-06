d90ba4deb15dd6a9ef07488e66732a41084ac21f
tbradsha <tbradsha@users.noreply.github.com>
2025-08-06 19:58:58 +0000

    Update dependency tmp to v0.2.4 [SECURITY] (#44667)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16786871649
    
    Upstream-Ref: Automattic/jetpack@8a5c82c2a079196cc391f78e02084548e35c1563

--- jetpack_vendor/automattic/jetpack-mu-wpcom/src/build/wpcom-dashboard-widgets/wpcom-dashboard-widgets.asset.php
+++ jetpack_vendor/automattic/jetpack-mu-wpcom/src/build/wpcom-dashboard-widgets/wpcom-dashboard-widgets.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '98bd2e18d27933bcc57b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '51405f0bba4d672bc83e');
