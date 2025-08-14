52b6774365ea3d7eb078e3c478bb6a85bda9f375
tbradsha <tbradsha@users.noreply.github.com>
2025-08-14 14:19:13 +0000

    Changelog and readme.txt edits. (#44799)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16967363902
    
    Upstream-Ref: Automattic/jetpack@fb9dc16e312ebc84b06685155f16ed3c4fa86a2f

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,7 +14,7 @@ return array(
     ),
     'jetpack-blaze' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-blaze',
-      'ver' => '0.26.3-alpha1755019340',
+      'ver' => '0.26.3',
     ),
     'jetpack-calypsoify' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-calypsoify',
@@ -22,7 +22,7 @@ return array(
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.13.15-alpha1755091645',
+      'ver' => '0.13.15',
     ),
     'jetpack-compat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-compat',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.18.0-alpha1755107315',
+      'ver' => '6.18.0',
     ),
     'jetpack-google-analytics' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-google-analytics',
@@ -42,11 +42,11 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.3-alpha1755091645',
+      'ver' => '4.3.3',
     ),
     'jetpack-masterbar' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-masterbar',
-      'ver' => '0.21.0-alpha1755168149',
+      'ver' => '0.21.0',
     ),
     'jetpack-mu-wpcom' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-mu-wpcom',
@@ -70,11 +70,11 @@ return array(
     ),
     'jetpack-subscribers-dashboard' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-subscribers-dashboard',
-      'ver' => '0.2.2-alpha1754976850',
+      'ver' => '0.2.2',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.18.2-alpha1755091645',
+      'ver' => '4.18.2',
     ),
   ),
 );
