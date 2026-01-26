af204a1cec1a19edd3a94c730617973099949e60
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2026-01-26 10:13:29 +0000

    Backport jetpack 15.5-a.5 changes (#46758)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21353487016
    
    Upstream-Ref: Automattic/jetpack@5987b386be2e301d736a49c92b5e0aa0797d2c57

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.20',
+      'ver' => '4.3.21',
     ),
     'jetpack-blaze' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-blaze',
-      'ver' => '0.27.0-alpha1769172818',
+      'ver' => '0.27.0',
     ),
     'jetpack-block-delimiter' => array(
       'path' => 'jetpack_vendor/automattic/block-delimiter',
@@ -22,7 +22,7 @@ return array(
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.14.10',
+      'ver' => '0.14.11',
     ),
     'jetpack-compat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-compat',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.20.4',
+      'ver' => '6.20.5',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.7',
+      'ver' => '0.4.8',
     ),
     'jetpack-google-analytics' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-google-analytics',
@@ -46,11 +46,11 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.16',
+      'ver' => '4.3.17',
     ),
     'jetpack-masterbar' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-masterbar',
-      'ver' => '0.27.8',
+      'ver' => '0.27.9',
     ),
     'jetpack-mu-wpcom' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-mu-wpcom',
@@ -70,15 +70,15 @@ return array(
     ),
     'jetpack-stats-admin' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats-admin',
-      'ver' => '0.30.4',
+      'ver' => '0.30.5',
     ),
     'jetpack-subscribers-dashboard' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-subscribers-dashboard',
-      'ver' => '0.4.14',
+      'ver' => '0.4.15',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.25.2-alpha1769190360',
+      'ver' => '4.25.2',
     ),
   ),
 );
