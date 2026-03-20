e5d42b9390aea872bbcdc0f2751faebdb854c9c8
simison <simison@users.noreply.github.com>
2026-03-20 11:30:52 +0000

    Update wordpress monorepo (#47684)
    * Update wordpress monorepo
    * Remove obsolete patch
    * Bump @types/* and fix various type errors
    * Add eslint-disables for overzealous `@wordpress/no-unknown-ds-tokens` lint
    
    ---------
    
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    Co-authored-by: William Viana <vianasw@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/23340310255
    
    Upstream-Ref: Automattic/jetpack@03439481ce6bb7bc3b21069e87bab244198a8f4b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.28',
+      'ver' => '4.3.29-alpha1774005138',
     ),
     'jetpack-blaze' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-blaze',
-      'ver' => '0.27.8',
+      'ver' => '0.27.9-alpha1774005138',
     ),
     'jetpack-block-delimiter' => array(
       'path' => 'jetpack_vendor/automattic/block-delimiter',
@@ -22,7 +22,7 @@ return array(
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.14.18',
+      'ver' => '0.14.19-alpha1774005138',
     ),
     'jetpack-compat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-compat',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.0.4',
+      'ver' => '8.0.5-alpha1774005138',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.16',
+      'ver' => '0.4.17-alpha1774005138',
     ),
     'jetpack-google-analytics' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-google-analytics',
@@ -50,19 +50,19 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.26',
+      'ver' => '4.3.27-alpha1774005138',
     ),
     'jetpack-masterbar' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-masterbar',
-      'ver' => '0.27.16',
+      'ver' => '0.27.17-alpha1774005138',
     ),
     'jetpack-mu-wpcom' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-mu-wpcom',
-      'ver' => '6.10.0-alpha1774003010',
+      'ver' => '6.10.0-alpha1774005138',
     ),
     'jetpack-newsletter' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-newsletter',
-      'ver' => '0.6.2-alpha1773822176',
+      'ver' => '0.6.2-alpha1774005138',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -86,7 +86,7 @@ return array(
     ),
     'jetpack-subscribers-dashboard' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-subscribers-dashboard',
-      'ver' => '0.4.22',
+      'ver' => '0.4.23-alpha1774005138',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
