01c6962cb0d9c5f55a5cde861032e61df2cc6f2b
arcangelini <arcangelini@users.noreply.github.com>
2026-07-08 10:43:25 +0000

    Podcast Episode: render the player in all contexts (#50286)
    * Podcast Episode: render the player in all contexts
    
    * Drop dead is_frontend test scaffolding after render simplification
    
    * Add linked title and native media fallback links for graceful degradation
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28935926405
    
    Upstream-Ref: Automattic/jetpack@6eac1e96909af43c6bf8ec44f36ce3118752f613

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-podcast' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-podcast',
-      'ver' => '1.3.2-alpha1783426613',
+      'ver' => '1.3.2-alpha1783506670',
     ),
     'jetpack-post-media' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-media',
