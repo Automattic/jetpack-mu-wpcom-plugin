9935a0e0a5717abed165ffc219fa0d000e356367
arcangelini <arcangelini@users.noreply.github.com>
2026-07-14 20:37:27 +0000

    Podcast: gate episode block properly (#50449)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29365642411
    
    Upstream-Ref: Automattic/jetpack@34f7e83e6c1b442b3d5d7ed0f145f51513f8a2c0

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-podcast' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-podcast',
-      'ver' => '1.3.2-alpha1784017885',
+      'ver' => '1.4.0-alpha1784060724',
     ),
     'jetpack-post-media' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-media',
