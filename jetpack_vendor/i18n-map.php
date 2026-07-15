6e1f6dbb1310900235bcf68654f1802e9354dd48
enejb <enejb@users.noreply.github.com>
2026-07-15 20:48:16 +0000

    Changelog and readme.txt edits. (#50551)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29448865409
    
    Upstream-Ref: Automattic/jetpack@e3407cc849e8f6dfea5b12944c65a3fde934f667

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.9',
+      'ver' => '0.9.10',
     ),
     'jetpack-agents-manager' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-agents-manager',
@@ -14,7 +14,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.4.5',
+      'ver' => '4.4.6-alpha1784147716',
     ),
     'jetpack-blaze' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-blaze',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.8',
+      'ver' => '8.7.9-alpha1784147716',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -54,11 +54,11 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.47',
+      'ver' => '4.3.48-alpha1784147716',
     ),
     'jetpack-masterbar' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-masterbar',
-      'ver' => '0.27.33',
+      'ver' => '0.27.34-alpha1784147716',
     ),
     'jetpack-mu-wpcom' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-mu-wpcom',
@@ -102,7 +102,7 @@ return array(
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
-      'ver' => '0.2.2',
+      'ver' => '0.2.3-alpha1784147716',
     ),
   ),
 );
