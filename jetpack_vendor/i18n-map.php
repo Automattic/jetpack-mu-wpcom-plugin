f1a1e26d27a83870adf8f5ad85b9590e9fd8b26d
arcangelini <arcangelini@users.noreply.github.com>
2026-07-15 22:18:14 +0000

    Podcast: remove the settings-saved analytics event (#50555)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29454212660
    
    Upstream-Ref: Automattic/jetpack@4c3e1c1aaf85131c3ea097b3cd19cdef55c29962

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-podcast' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-podcast',
-      'ver' => '1.4.0-alpha1784152605',
+      'ver' => '1.4.0-alpha1784153127',
     ),
     'jetpack-post-media' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-media',
