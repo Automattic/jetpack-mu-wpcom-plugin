21c2bac43c49db7190822ab8a02b142e9c1ef119
arcangelini <arcangelini@users.noreply.github.com>
2026-07-15 08:44:57 +0000

    Podcast: add soundbites and alternate files editor controls (#50513)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29401153477
    
    Upstream-Ref: Automattic/jetpack@a0953d6b90739ed4cae048cb025c6dd0e3156da0

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-podcast' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-podcast',
-      'ver' => '1.4.0-alpha1784103012',
+      'ver' => '1.4.0-alpha1784104346',
     ),
     'jetpack-post-media' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-media',
