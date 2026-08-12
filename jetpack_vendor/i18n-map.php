23fb3153397537bc813370e251204bddb862402b
chihsuan <chihsuan@users.noreply.github.com>
2026-08-12 05:47:11 +0000

    Premium Analytics: extract the shared calendar heatmap tile sizing (#51161)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/31566874798
    
    Upstream-Ref: Automattic/jetpack@9b552541bc738de94d79582103df8907a614cc2f

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -86,7 +86,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.3.0-alpha1786507404',
+      'ver' => '0.3.0-alpha1786512742',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
