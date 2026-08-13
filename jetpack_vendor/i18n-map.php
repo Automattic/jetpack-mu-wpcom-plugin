6e15afe388389baced20d9ae28f3b8ab7396645c
chihsuan <chihsuan@users.noreply.github.com>
2026-08-13 07:37:28 +0000

    feat: give the Insights traffic views heatmap two rows by default (#51240)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/31677551360
    
    Upstream-Ref: Automattic/jetpack@488e4634d9ebc5a66a7064d83e126f41ac4accd8

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -86,7 +86,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.3.0-alpha1786599240',
+      'ver' => '0.3.0-alpha1786605794',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
