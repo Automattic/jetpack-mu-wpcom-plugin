ca3edca8a143ae55c70d9f298cf04d683bc4ae4d
retrofox <retrofox@users.noreply.github.com>
2026-08-18 06:02:29 +0000

    Premium Analytics: pick the chart type from an icon toggle (#51337)
    * add chartLine icon to the analytics icons package
    
    * add ToggleGroupField DataForm control
    
    renders a field's elements as segments, icon or text
    
    * fill the selected ToggleGroupField segment
    
    dark indicator and inverted glyph for contrast
    
    * use ToggleGroupField for the chart type field
    
    replaces the dropdown in every chart widget header
    
    * add changelog entry
    
    * fall back to the first ToggleGroupField option
    
    an absent or unknown value left every segment unpressed
    
    * use a userEvent instance in the ToggleGroupField tests
    
    matches every other userEvent test in the package
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32104266724
    
    Upstream-Ref: Automattic/jetpack@9ca98559cfae082a8776e550f153310b4368cf2c

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -90,7 +90,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.3.0-alpha1787031789',
+      'ver' => '0.3.0-alpha1787032060',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
