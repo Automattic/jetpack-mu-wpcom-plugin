28b23e3b1b713090992b75f333fc109e5b1b238f
anomiex <anomiex@users.noreply.github.com>
2026-08-20 17:17:21 +0000

    General: Update minimum WordPress version to 7.0 (#51370)
    WordPress 7.1 shipped on 2026-08-19, which makes 7.0 the previous major
    and the new floor for the monorepo's plugins.
    
    Bumps the "Requires at least" headers, JETPACK__MINIMUM_WP_VERSION,
    MIN_WP_VERSION in .github/versions.sh, the PHPCS
    minimum_supported_wp_version, the WordPress stub constraints, and the
    readme.txt template used by `jetpack generate`.
    
    We also have to bring php-stubs/wp-cli-stubs dev-master, as upstream
    refuses to release a new version to fix their php-stubs/wordpress-stubs
    dep (at "^6.0", so it won't allow 7.0) until a new version of wp-cli is
    released.
    
    Then clean up some straightforward code, comments, and linter
    suppressions that are obsolete now that we dropped support for 6.9.
    
    We also drop the PHP 7.2 and 7.3 test jobs, as WordPress 7.0 no longer
    supports those versions. Dropping support for those in the monorepo
    itself will be in a future PR.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32395412905
    
    Upstream-Ref: Automattic/jetpack@bfc5492d6263578a63cfacf4ec0af3f495df7943

--- jetpack_vendor/automattic/jetpack-mu-wpcom/src/build/wpcom-blocks-code-block-definition/wpcom-blocks-code-block-definition.asset.php
+++ jetpack_vendor/automattic/jetpack-mu-wpcom/src/build/wpcom-blocks-code-block-definition/wpcom-blocks-code-block-definition.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => '54123cc463dd3407ace9');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => 'bd0f101cd4a7370470e0');
--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.11.0-alpha1787222251',
+      'ver' => '8.11.0-alpha1787245368',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -74,7 +74,7 @@ return array(
     ),
     'jetpack-mu-wpcom' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-mu-wpcom',
-      'ver' => '6.11.0-alpha1787225264',
+      'ver' => '6.11.0-alpha1787245368',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
