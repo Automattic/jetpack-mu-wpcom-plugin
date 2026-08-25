b191b3561a0b92f2a6b48a8f7a29995a75dec2a1
chihsuan <chihsuan@users.noreply.github.com>
2026-08-25 07:37:27 +0000

    perf(premium-analytics): group the jest suites that declare no module mocks (#51537)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32821443833
    
    Upstream-Ref: Automattic/jetpack@19bfbc38a56cd7e5f8c85656de6af89a584c837f

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.4.0-alpha1787641981',
+      'ver' => '0.4.0-alpha1787642668',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
