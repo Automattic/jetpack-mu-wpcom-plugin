d4e0cb854f6b64f604c1236004f622b3d5fe64f0
chihsuan <chihsuan@users.noreply.github.com>
2026-08-26 09:49:18 +0000

    Premium Analytics: match Latest subscribers row metrics to the design (#51597)
    * style: match Premium Analytics subscriber list rows to the design
    
    * style: use the WPDS gap token for subscriber list row separation
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32953666802
    
    Upstream-Ref: Automattic/jetpack@563e5949933e41bd51b13b8818a7da02c7eb0d6d

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.5.0-alpha1787736786',
+      'ver' => '0.5.0-alpha1787736826',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
