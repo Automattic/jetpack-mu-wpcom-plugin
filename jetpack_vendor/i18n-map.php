aa60d2d614dee3aeac009cde79e77e4d21fca09f
simison <simison@users.noreply.github.com>
2026-08-26 07:37:25 +0000

    Podcasting: use Linkbutton instead of Button for links (#51559)
    Replaces the workaround of using a Button for links with the appropriate new LinkButton component.
    
    ---------
    
    Co-authored-by: Tony Arcangelini <tony@arcangelini.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32942469344
    
    Upstream-Ref: Automattic/jetpack@16a2d69626fbd32c482991d39d7da1ed1df20669

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -86,7 +86,7 @@ return array(
     ),
     'jetpack-podcast' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-podcast',
-      'ver' => '2.0.0-alpha1787686674',
+      'ver' => '2.0.0-alpha1787728966',
     ),
     'jetpack-post-media' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-media',
