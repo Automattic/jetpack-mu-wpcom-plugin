05b836ae8078a2a8db6a13bc23dd489d25d15539
Nikschavan <Nikschavan@users.noreply.github.com>
2026-08-27 06:35:05 +0000

    WOOA7S-1968: Open a chart bucket by clicking it (#51544)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33045591967
    
    Upstream-Ref: Automattic/jetpack@5f393484fd23c99bb46291206a1c915d62dae523

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.5.0-alpha1787804521',
+      'ver' => '0.5.0-alpha1787811708',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
