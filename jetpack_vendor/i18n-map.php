da091b3933a288a6215783edcb83c2726eb141df
retrofox <retrofox@users.noreply.github.com>
2026-09-01 08:51:37 +0000

    name the active interval in the trigger tooltip (#51770)
    the section header subtitle used to be the only place it was readable
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33487974169
    
    Upstream-Ref: Automattic/jetpack@7d9d2a1485eb591713d92c90d85ec3eaa4ff9512

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.5.0-alpha1788238980',
+      'ver' => '0.5.0-alpha1788251837',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
