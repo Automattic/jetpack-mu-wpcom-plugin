d3386de0cf9a89cac54b902404e5ebd707791bef
Nikschavan <Nikschavan@users.noreply.github.com>
2026-09-03 08:44:55 +0000

    WOOA7S-1937: Match current Stats date range presets (#51428)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33733715491
    
    Upstream-Ref: Automattic/jetpack@3e5ed59260b393ed73635709958136d08e8dfc56

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.6.0-alpha1788422847',
+      'ver' => '0.6.0-alpha1788424222',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
