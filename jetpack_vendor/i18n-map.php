5f7fef69e7d0f994a2c8770f33d81607047791c1
layoutd <layoutd@users.noreply.github.com>
2026-09-03 16:20:00 +0000

    Premium Analytics: cover embedded quotes in report CSV generator tests (#51874)
    * premium-analytics: cover embedded quotes in CSV generator tests
    
    * premium-analytics: add test-only changelog entry
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33776648652
    
    Upstream-Ref: Automattic/jetpack@2bef632f7d0840e842a6e908f5e46313935e8e1e

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.6.0-alpha1788444461',
+      'ver' => '0.6.0-alpha1788451594',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
