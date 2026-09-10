9e2865f5f4e3ec917b42d56860e69309b8e0aa78
chihsuan <chihsuan@users.noreply.github.com>
2026-09-10 09:32:00 +0000

    Premium Analytics: hide the dashboard date controls while customizing (#52176)
    * fix: hide the Premium Analytics dashboard date controls while customizing
    
    * chore: drop the wpcomsh changelog entry, Atomic reaches the dashboard through the Jetpack plugin
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34459659710
    
    Upstream-Ref: Automattic/jetpack@ad50de5bf4ff019d9389a1ca56119dfffb29c788

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.7.0-alpha1789028199',
+      'ver' => '0.7.0-alpha1789031740',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
