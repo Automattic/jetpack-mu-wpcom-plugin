ddc8f621fa4fd96b098ab48956f417d64bd77ebf
retrofox <retrofox@users.noreply.github.com>
2026-09-14 11:28:25 +0000

    Premium Analytics: extract WidgetRoot report params into a hook (#51305)
    * refactor WidgetRoot report params into a hook
    
    shared resolution for surfaces outside WidgetRoot
    
    * add changelog for report params hook refactor
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34836979754
    
    Upstream-Ref: Automattic/jetpack@a5cf2ac9e84510d78f60e443bb742c43820570d3

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.7.0-alpha1789380315',
+      'ver' => '0.7.0-alpha1789384313',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
