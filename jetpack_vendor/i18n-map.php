6170a90f5c48c73d25d905e56009fe9affb93be9
chihsuan <chihsuan@users.noreply.github.com>
2026-09-15 05:07:17 +0000

    Premium Analytics: align the Year in review tile icons and footer spacing with the design (#52307)
    * fix: match the Year in review tile icons to the prototype and space the tiles off the footer
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34930528219
    
    Upstream-Ref: Automattic/jetpack@d01c0f8b24ac281ba972b7e32c5c00ffb7096bb7

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.7.0-alpha1789447022',
+      'ver' => '0.7.0-alpha1789447967',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
