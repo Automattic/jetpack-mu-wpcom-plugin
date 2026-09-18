ba40fad089a985e32cbf49fda7467ee583f5bdbc
retrofox <retrofox@users.noreply.github.com>
2026-09-18 14:16:45 +0000

    cover the dashboard section hooks with tests (#52486)
    useActiveSection and useDashboardSections had no suite
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35353499451
    
    Upstream-Ref: Automattic/jetpack@f3097978f94a3e5ba49d0f6209566d1903a215fd

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.8.0-alpha1789737152',
+      'ver' => '0.8.0-alpha1789739984',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
