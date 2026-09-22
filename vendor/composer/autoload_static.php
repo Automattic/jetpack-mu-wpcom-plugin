a11f4a22bcc66e1385117bfc70232d7273295d58
Nikschavan <Nikschavan@users.noreply.github.com>
2026-09-22 10:50:55 +0000

    STATS-479: Stats: move the Stats settings to a Settings tab in the Stats dashboard (#52540)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35716770953
    
    Upstream-Ref: Automattic/jetpack@c7420ed5a57bfe0d460945194a72b8c347f4b8ed

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -371,6 +371,7 @@ class ComposerStaticInitd9d132a783958a00a2c7cccff60ca42d_jetpack_mu_wpcom_plugin
         'Automattic\\Jetpack\\Stats\\Options' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-stats/src/class-options.php',
         'Automattic\\Jetpack\\Stats\\Package_Version' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-stats/src/class-package-version.php',
         'Automattic\\Jetpack\\Stats\\REST_Provider' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-stats/src/class-rest-provider.php',
+        'Automattic\\Jetpack\\Stats\\Settings' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-stats/src/class-settings.php',
         'Automattic\\Jetpack\\Stats\\Tracking_Pixel' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-stats/src/class-tracking-pixel.php',
         'Automattic\\Jetpack\\Stats\\Transient_Cleanup' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-stats/src/class-transient-cleanup.php',
         'Automattic\\Jetpack\\Stats\\WPCOM_Stats' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-stats/src/class-wpcom-stats.php',
