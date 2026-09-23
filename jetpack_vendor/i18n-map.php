c7a1fb4696eb5181de510c8c5096860aa0e853b8
fgiannar <fgiannar@users.noreply.github.com>
2026-09-23 07:52:16 +0000

    Connection: Track reconnect and link clicks in the connection error notice (#52617)
    * Connection: Track reconnect and link clicks in the connection error notice
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35832813410
    
    Upstream-Ref: Automattic/jetpack@4315f2239752077afb961aee092dbdea56348274

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.6.0-alpha1790095377',
+      'ver' => '9.6.0-alpha1790149132',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
